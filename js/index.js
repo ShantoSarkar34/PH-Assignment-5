cd9779607e5840a1db4628bc15d7adffbe0b8c29
ShantoSarkar34 <shanto9070sarkar@gmail.com>
2025-03-01 01:38:00 +0600

    header and hero section done

---
+++ js/index.js
@@ -0,0 +1,66 @@
+document
+  .getElementById("bg_color_change")
+  .addEventListener("click", function () {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+  });
+
+document
+  .getElementById("second_page_btn")
+  .addEventListener("click", function () {
+    window.location.href = "./second.html";
+  });
+
+function date() {
+  const today = new Date();
+  const options = {
+    weekday: "short",
+    month: "short",
+    day: "2-digit",
+    year: "numeric",
+  };
+  const currentDate = today.toLocaleDateString("en-US", options);
+  document.getElementById("current_date").innerText = currentDate;
+}
+date();
+
+const compleateButtons = document.querySelectorAll(".compleate_btn");
+
+compleateButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    button.disabled = true;
+    button.style.backgroundColor = "gray";
+    alert("Board update seccessfuly");
+
+    const totalTask = document.getElementById("total_task").innerText;
+    let convertTotalTask = parseInt(totalTask);
+
+    const remainingClick = document.getElementById("remaining_click").innerText;
+    let convertClick = parseInt(remainingClick);
+
+
+    if (convertClick > 0 && convertTotalTask > 0) {
+      convertClick--;
+      document.getElementById("remaining_click").innerText = convertClick;
+      convertTotalTask++;
+      document.getElementById("total_task").innerText = convertTotalTask;
+
+      document.getElementById("history").innerHTML = `
+      <h2 class="text-[16px] opacity-[70%] text-[#00303C]">you have compleated the task</h2>
+      `;
+      
+      if (convertClick < 1) {
+        alert("Congrats!! You have compleated all the Task");
+      }
+    }
+  });
+});
+
+document
+  .getElementById("history_clear_btn")
+  .addEventListener("click", function () {
+    document.getElementById("history").style.display = "none";
+  });
