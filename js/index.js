8823e33c9d93fc59bc16da575e538e5732cf172e
ShantoSarkar34 <shanto9070sarkar@gmail.com>
2025-03-01 15:58:33 +0600

    final comit

--- js/index.js
+++ js/index.js
@@ -32,7 +32,8 @@ const compleateButtons = document.querySelectorAll(".compleate_btn");
 compleateButtons.forEach((button) => {
   button.addEventListener("click", function () {
     button.disabled = true;
-    button.style.backgroundColor = "gray";
+    button.style.opacity = "20%";
+    button.style.cursor = "not-allowed";
     alert("Board update seccessfuly");
 
     const totalTask = document.getElementById("total_task").innerText;
@@ -41,31 +42,11 @@ compleateButtons.forEach((button) => {
     const remainingClick = document.getElementById("remaining_click").innerText;
     let convertClick = parseInt(remainingClick);
 
-    const cartTitle = document.querySelector("h3").textContent;
-
-    const currentTime = new Date().toLocaleTimeString();
-    const mainHistory = document.getElementById("history");
-
-    const newElement = document.createElement("h4");
-    document
-      .getElementById("history")
-      .classList.add(
-        "text-[16px]",
-        "p-4",
-        "opacity-[70%]",
-        "bg-[#F4F7FF]",
-        "rounded-[12px]"
-      );
-
     if (convertClick > 0 && convertTotalTask > 0) {
       convertClick--;
       document.getElementById("remaining_click").innerText = convertClick;
       convertTotalTask++;
       document.getElementById("total_task").innerText = convertTotalTask;
-
-      newElement.textContent = `You have compleated the task ${cartTitle}  ${currentTime}`;
-      mainHistory.appendChild(newElement);
-
       if (convertClick < 1) {
         alert("Congrats!! You have compleated all the Task");
       }
@@ -79,3 +60,18 @@ document
     document.getElementById("history").innerHTML = "";
     document.getElementById("history").classList.remove("bg-[#F4F7FF]");
   });
+
+function logName(button) {
+  const card = button.closest(".card");
+  const name = card.querySelector(".card_title").textContent;
+  const history = document.getElementById("history");
+  const currentTime = new Date().toLocaleTimeString();
+  newTitle = name;
+
+  const newEntry = document.createElement("div");
+  newEntry.innerHTML = `<div class="p-4 bg-[#F4F7FF] rounded-[12px]">
+  <h4 class="text-[16px] opacity-[70%]">You have compleated the task ${newTitle} ${currentTime}</h4>
+  </div>`;
+
+  history.appendChild(newEntry);
+}
